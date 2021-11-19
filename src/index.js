29383f38d8e1c10594283a4ae045eaf9642bb089
Edgar Uco <edgarin@gmail.com>
2021-11-19 16:20:11 -0600

    practica mongodb despliegue local

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// Imports
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+const path = require('path');
+
+const connectDB = require('./config/db');
+
+const bodyParser = require('body-parser');
+
+// Middlewares
+// Es una funcion que se ejecuta despues de recibir una peticion y antes de dar una respuesta
+// Trabajar con archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configuraciones
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+connectDB();
+// Routes
+// Home
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Animes
+app.use('/animes', require('./routes/animes.router'));
+
+// Server
+app.listen(process.env.PORT, () => {
+    console.log(`Server on port: http://localhost:${process.env.PORT}`);
+});
---
+++ src/routes/animes.router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+//const animeController = require('./../controllers/animes.controller');
+const animeController = require('./../controllers/animes-controller');
+
+// Routes
+// Obtener los animes o mostrarlos en pantalla
+router.get('/', animeController.getAnimes);
+router.put('/:id/update', animeController.putAnimes);
+
+module.exports = router;
